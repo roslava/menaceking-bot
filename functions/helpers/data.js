9a02ab835ea759ca0ce5e1c120cdb00cc1f385be
Rostislav Nenashev <roslav@icloud.com>
2025-05-02 17:56:51 +0300

    remove functions

--- functions/helpers/data.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    trafficOptions: [
-        'SEO', 'Telegram', 'Streams', 'CPA Team',
-        'Social', 'PPC', 'Apps / ASO / UAC',
-        'SMS / Mailing', 'Facebook', 'TikTok', 'Networks', 'Other'
-    ],
-    geoOptions: ['World Wide', 'Asia', 'CIS', 'Tier-1', 'Latam', 'Africa']
-};
--- functions/steps.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    LANGUAGE: 'language',
-    CONTACT: 'contact',
-    COMPANY: 'company',
-    TRAFFIC: 'traffic',
-    GEO: 'geo',
-    CONFIRM: 'confirm',
-    DONE: 'done',
-};
